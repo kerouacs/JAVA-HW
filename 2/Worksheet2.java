f6f65693d1d47ec90240c9d586d7b5025cff2ab6
Your Name <543068858@qq.com>
2020-01-27 23:25:32 +0000

    1.27

--- 2/Worksheet2.java
+++ 2/Worksheet2.java
@@ -1,93 +1,112 @@
 /**
- * @author <PUT YOUR NAME HERE>
- * This class contains the solution for Worksheet2
+ * @author <PUT YOUR NAME HERE> This class contains the solution for Worksheet2
  */
 
 public class Worksheet2 implements Worksheet2Interface {
 
-	private static List result;
+
 	// Exercise 1
 
 	static Tree<Integer> negateAll(Tree<Integer> t) {
-		if(t.isEmpty()) {
+		if (t.isEmpty()) {
 			return t;
 		}
-		return new Tree<Integer>(t.value*-1,negateAll(t.getLeft()),negateAll(t.getRight()));
+		return new Tree<Integer>(t.value * -1, negateAll(t.getLeft()), negateAll(t.getRight()));
 	}
 
 	// Exercise 2
 
 	static boolean allEven(Tree<Integer> a) {
-		if(a.isEmpty()) {
+		if (a.isEmpty()) {
 			return true;
 		}
-		if(a.getValue()%2==0) {
-			return allEven(a.getLeft())&&allEven(a.getRight());
-		}else return false;
+		if (a.getValue() % 2 == 0) {
+			return allEven(a.getLeft()) && allEven(a.getRight());
+		} else
+			return false;
 
 	}
 
 	// Exercise 3
 
-	static int depth(Tree<Integer> a, int x) {	
-			return getDepth(a, x, 0);
-	}
-	static int getDepth(Tree<Integer> a, int x, int dep) {
-		if (a.isEmpty()) return -1;
+	static int depth(Tree<Integer> a, int x) {
+		if (a.isEmpty())
+			return -1;
 		if (a.value == x) {
-			return dep;
+			return 0;
 		}
-		return Math.max(getDepth(a.getLeft(), x, dep + 1),
-				getDepth(a.getRight(), x, dep + 1));
-
-
+		return Math.max(depth(a.getLeft(), x) == -1 ? -1 : 1 + depth(a.getLeft(), x),
+				depth(a.getRight(), x) == -1 ? -1 : 1 + depth(a.getRight(), x));
 	}
+
 	// Exercise 4
 
 	static <E> List<E> preorder(Tree<E> a) {
 		if (a.isEmpty()) {
 			return new List<>();
 		}
-		List<E> ar = new List<>();
-		List<E> al = new List<>();
+		List<E> r = new List<>();
+		List<E> l = new List<>();
 		if (!a.right.isEmpty()) {
-			 ar = preorder(a.right);
+			 r = preorder(a.right);
 		}
 		if (!a.left.isEmpty()) {
-			 al = preorder(a.left);
+			 l = preorder(a.left);
 		}
-//		List<E> lr = merge(al, ar);
-//		List<E> res = new List(a.value, lr);
-		return new List<>(a.value, merge(al, ar));
+		return new List<>(a.value, merge(l, r));
 	}
 
-	/**
-	 *
-	 * @param al The list from the left subtree.
-	 * @param ar The list from the right subtree.
-	 * @param <E> The generic type of the List.
-	 * @return The merge list of two list from subtrees.
-	 */
-	private static <E> List<E> merge(List<E> al, List<E> ar) {
-		if (al.isEmpty()) return ar;
-		return new List<>(al.getHead(), merge(al.getTail(), ar));
+
+	private static <E> List<E> merge(List<E> l, List<E> r) {
+		if (l.isEmpty()) return r;
+		return new List<>(l.getHead(), merge(l.getTail(), r));
 	}
+
 
 	// Exercise 5
 
 	static boolean isSearchTree(Tree<Integer> a) {
-		return true;
+		return helpMethod(a,0);
 	}
+	
+	
+	static boolean helpMethod(Tree<Integer> a,int last) {
+    if(a.isEmpty())
+        return true;
+    if(!isSearchTree(a.getLeft()))
+        return false;
+    if(a.getValue()< last)
+        return false;
+    last = a.getValue();
+    if(!isSearchTree(a.getRight()))
+        return false;
+    return true;
+	}
+    
+
 
 	// Exercise 6
 
 	static void printDescending(Tree<Integer> a) {
+		if (a.isEmpty() ) {
+			return;
+		}
+		printDescending(a.right);
+		System.out.println(a.value);
+		printDescending(a.left);
 	}
+	
 
 	// Exercise 7
 
 	static int max(Tree<Integer> a) {
-		return 0;
+		if(a.isEmpty()) {
+			return 0;
+		}
+		if(a.getRight().isEmpty()) {
+			return a.getValue();
+		}
+		return max(a.getRight());
 	}
 
 	// Exercise 8
@@ -110,19 +129,22 @@ public class Worksheet2 implements Worksheet2Interface {
 	static Tree<Integer> deleteHB(Tree<Integer> a, int x) {
 		return new Tree<Integer>();
 	}
-	static void printList (List <Integer> L) {
+
+	static void printList(List<Integer> L) {
 		if (L.isEmpty())
-		return;
+			return;
 		else {
-		System.out.println(L.getHead());
-		printList(L.getTail());
+			System.out.println(L.getHead());
+			printList(L.getTail());
 		}
-		}
-	public static void main(String[] args) {
-		Tree a=new Tree<> (5,new Tree<>(3,new Tree<>(1),new Tree<>(4)),new Tree<>(8,new Tree<>(6),new Tree<>()));
-//		Tree.print(a);
-		printList(preorder(a));
 	}
-		
+
+	public static void main(String[] args) {
+		Tree a = new Tree<>(5, new Tree<>(3, new Tree<>(1), new Tree<>(4)), new Tree<>(8, new Tree<>(6), new Tree<>()));
+//		Tree.print(a);
+//		printList(preorder(a));
+		System.out.println(max(a));
+//		printDescending(a);
+	}
+
 }
-	
