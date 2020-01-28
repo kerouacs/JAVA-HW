c98609f9856db715bbfd1adf1754a1c35e380252
Your Name <543068858@qq.com>
2020-01-28 23:31:56 +0000

    1.28

--- 2/Worksheet2.java
+++ 2/Worksheet2.java
@@ -1,9 +1,8 @@
 /**
- * @author <PUT YOUR NAME HERE> This class contains the solution for Worksheet2
+ * @author <Ruopeng Jiang> This class contains the solution for Worksheet2
  */
 
 public class Worksheet2 implements Worksheet2Interface {
-
 
 	// Exercise 1
 
@@ -48,62 +47,57 @@ public class Worksheet2 implements Worksheet2Interface {
 		List<E> r = new List<>();
 		List<E> l = new List<>();
 		if (!a.right.isEmpty()) {
-			 r = preorder(a.right);
+			r = preorder(a.right);
 		}
 		if (!a.left.isEmpty()) {
-			 l = preorder(a.left);
+			l = preorder(a.left);
 		}
 		return new List<>(a.value, merge(l, r));
 	}
 
-
 	private static <E> List<E> merge(List<E> l, List<E> r) {
-		if (l.isEmpty()) return r;
+		if (l.isEmpty())
+			return r;
 		return new List<>(l.getHead(), merge(l.getTail(), r));
 	}
-
 
 	// Exercise 5
 
 	static boolean isSearchTree(Tree<Integer> a) {
-		return helpMethod(a,0);
+		return helpMethod(a, 0);
 	}
-	
-	
-	static boolean helpMethod(Tree<Integer> a,int last) {
-    if(a.isEmpty())
-        return true;
-    if(!isSearchTree(a.getLeft()))
-        return false;
-    if(a.getValue()< last)
-        return false;
-    last = a.getValue();
-    if(!isSearchTree(a.getRight()))
-        return false;
-    return true;
-	}
-    
 
+	static boolean helpMethod(Tree<Integer> a, int last) {
+		if (a.isEmpty())
+			return true;
+		if (!isSearchTree(a.getLeft()))
+			return false;
+		if (a.getValue() < last)
+			return false;
+		last = a.getValue();
+		if (!isSearchTree(a.getRight()))
+			return false;
+		return true;
+	}
 
 	// Exercise 6
 
 	static void printDescending(Tree<Integer> a) {
-		if (a.isEmpty() ) {
+		if (a.isEmpty()) {
 			return;
 		}
-		printDescending(a.right);
-		System.out.println(a.value);
-		printDescending(a.left);
+		printDescending(a.getRight());
+		System.out.println(a.getValue());
+		printDescending(a.getLeft());
 	}
-	
 
 	// Exercise 7
 
 	static int max(Tree<Integer> a) {
-		if(a.isEmpty()) {
+		if (a.isEmpty()) {
 			return 0;
 		}
-		if(a.getRight().isEmpty()) {
+		if (a.getRight().isEmpty()) {
 			return a.getValue();
 		}
 		return max(a.getRight());
@@ -112,13 +106,44 @@ public class Worksheet2 implements Worksheet2Interface {
 	// Exercise 8
 
 	static Tree<Integer> delete(Tree<Integer> a, int x) {
-		return new Tree<Integer>();
+		if (a.isEmpty()) {
+			return null;
+		}
+		if (a.getValue() < x) {
+			return new Tree<Integer>(a.getValue(), a.getLeft(), delete(a.getRight(), x));
+		}
+		if (a.getValue() > x) {
+			return new Tree<Integer>(a.getValue(), delete(a.getLeft(), x), a.getRight());
+		} else {
+			if (a.getLeft().isEmpty()) {
+				return a.getRight();
+			} else if (a.getRight().isEmpty()) {
+				return a.getLeft();
+			} else {
+				return new Tree<>(max(a.getLeft()), delete(a.getLeft(), max(a.getLeft())), a.getRight());
+			}
+		}
+
 	}
 
 	// Exercise 9
 	static <E> boolean isHeightBalanced(Tree<E> a) {
-		return true;
+        if(a.isEmpty()) return true;
+        int left=getDepth(a.getLeft(),1);
+        int right=getDepth(a.getRight(),1);
+
+        return Math.abs(left-right)<=1&&isHeightBalanced(a.getLeft())&&isHeightBalanced(a.getRight());
+
 	}
+	
+    static <E> int getDepth(Tree<E> a,int depth){
+        if(a.isEmpty()) return depth;
+        
+        int left=getDepth(a.getLeft(),depth+1);
+        int right=getDepth(a.getRight(),depth+1);
+        return Math.max(left,right);
+   
+    }
 
 	// Exercise 10
 
@@ -141,9 +166,9 @@ public class Worksheet2 implements Worksheet2Interface {
 
 	public static void main(String[] args) {
 		Tree a = new Tree<>(5, new Tree<>(3, new Tree<>(1), new Tree<>(4)), new Tree<>(8, new Tree<>(6), new Tree<>()));
-//		Tree.print(a);
+//		Tree.print(delete(a, 5));
 //		printList(preorder(a));
-		System.out.println(max(a));
+		System.out.println(isHeightBalanced(a));
 //		printDescending(a);
 	}
 
