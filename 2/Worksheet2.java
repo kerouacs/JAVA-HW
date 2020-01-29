892caa652b12665eebd3dbbb9f5f64ef8082737b
Your Name <543068858@qq.com>
2020-01-29 23:56:57 +0000

    1.29

--- 2/Worksheet2.java
+++ 2/Worksheet2.java
@@ -5,16 +5,29 @@
 public class Worksheet2 implements Worksheet2Interface {
 
 	// Exercise 1
-
-	static Tree<Integer> negateAll(Tree<Integer> t) {
-		if (t.isEmpty()) {
-			return t;
+	/**
+	 * method that returns a new tree containing all the elements of a with their
+	 * sign negated.
+	 * 
+	 * @param a Given a tree of integers.
+	 * @return a new tree containing all the elements of a with their sign negated
+	 */
+	static Tree<Integer> negateAll(Tree<Integer> a) {
+		if (a.isEmpty()) {
+			return new Tree<>();
 		}
-		return new Tree<Integer>(t.value * -1, negateAll(t.getLeft()), negateAll(t.getRight()));
+		return new Tree<Integer>(a.getValue() * -1, negateAll(a.getLeft()), negateAll(a.getRight()));
 	}
 
 	// Exercise 2
-
+	/**
+	 * method that return a boolean value indicating whether all the values in its
+	 * nodes are even.
+	 * 
+	 * @param a Given a tree of integers.
+	 * @return a boolean value indicating whether all the values in its nodes are
+	 *         even.
+	 */
 	static boolean allEven(Tree<Integer> a) {
 		if (a.isEmpty()) {
 			return true;
@@ -27,11 +40,17 @@ public class Worksheet2 implements Worksheet2Interface {
 	}
 
 	// Exercise 3
-
+	/**
+	 * method that returns the depth of the given node value nodes are even.
+	 * 
+	 * @param a Given a tree of integers.
+	 * @param x Given a integer.
+	 * @return returns the depth of the given node value nodes are even.
+	 */
 	static int depth(Tree<Integer> a, int x) {
 		if (a.isEmpty())
 			return -1;
-		if (a.value == x) {
+		if (a.getValue() == x) {
 			return 0;
 		}
 		return Math.max(depth(a.getLeft(), x) == -1 ? -1 : 1 + depth(a.getLeft(), x),
@@ -39,22 +58,36 @@ public class Worksheet2 implements Worksheet2Interface {
 	}
 
 	// Exercise 4
-
+	/**
+	 * method that return a list containing the values in a by traversing the nodes
+	 * in preorder.
+	 * 
+	 * @param a Given a tree of integers.
+	 * @return return a list containing the values in a by traversing the nodes in
+	 *         preorde
+	 */
 	static <E> List<E> preorder(Tree<E> a) {
 		if (a.isEmpty()) {
 			return new List<>();
 		}
 		List<E> r = new List<>();
 		List<E> l = new List<>();
-		if (!a.right.isEmpty()) {
-			r = preorder(a.right);
+		if (!a.getRight().isEmpty()) {
+			r = preorder(a.getRight());
 		}
-		if (!a.left.isEmpty()) {
-			l = preorder(a.left);
+		if (!a.getLeft().isEmpty()) {
+			l = preorder(a.getLeft());
 		}
-		return new List<>(a.value, merge(l, r));
+		return new List<>(a.getValue(), merge(l, r));
 	}
 
+	/**
+	 * method that return two lists merged
+	 * 
+	 * @param l Given a first list.
+	 * @param r Given a second list.
+	 * @return return a merged list
+	 */
 	private static <E> List<E> merge(List<E> l, List<E> r) {
 		if (l.isEmpty())
 			return r;
@@ -62,26 +95,44 @@ public class Worksheet2 implements Worksheet2Interface {
 	}
 
 	// Exercise 5
-
+	/**
+	 * method that returns a boolean value indicating whether a is a binary search
+	 * tree.
+	 * 
+	 * @param a Given a tree of integers.
+	 * @return returns a boolean value indicating whether a is a binary search tree.
+	 */
 	static boolean isSearchTree(Tree<Integer> a) {
-		return helpMethod(a, 0);
+		return helpMethod(a, Integer.MIN_VALUE, Integer.MAX_VALUE);
 	}
 
-	static boolean helpMethod(Tree<Integer> a, int last) {
-		if (a.isEmpty())
+	/**
+	 * method that returns a boolean value indicating whether a is a binary search
+	 * tree.
+	 * 
+	 * @param a      Given a tree of integers.
+	 * @param minVal min of integers.
+	 * @param maxVal max of integers.
+	 * @return returns a boolean value indicating whether a is a binary search tree.
+	 */
+	static boolean helpMethod(Tree<Integer> a, int minVal, int maxVal) {
+		if (a.isEmpty()) {
 			return true;
-		if (!isSearchTree(a.getLeft()))
-			return false;
-		if (a.getValue() < last)
-			return false;
-		last = a.getValue();
-		if (!isSearchTree(a.getRight()))
-			return false;
-		return true;
+		}
+		if (a.getValue() > minVal && a.getValue() < maxVal) {
+			return helpMethod(a.getLeft(), minVal, a.getValue()) && helpMethod(a.getRight(), a.getValue(), maxVal);
+		}
+
+		return false;
 	}
 
 	// Exercise 6
-
+	/**
+	 * method that prints the values stored in it in descending order
+	 * 
+	 * @param a Given a tree of integers.
+	 * @return prints the values stored in it in descending order
+	 */
 	static void printDescending(Tree<Integer> a) {
 		if (a.isEmpty()) {
 			return;
@@ -92,7 +143,12 @@ public class Worksheet2 implements Worksheet2Interface {
 	}
 
 	// Exercise 7
-
+	/**
+	 * method that find the maximum value stored in the tree
+	 * 
+	 * @param a Given a tree of integers.
+	 * @return the maximum value stored in the tree
+	 */
 	static int max(Tree<Integer> a) {
 		if (a.isEmpty()) {
 			return 0;
@@ -104,7 +160,13 @@ public class Worksheet2 implements Worksheet2Interface {
 	}
 
 	// Exercise 8
-
+	/**
+	 * method that delete the value x from a and return the resulting tree
+	 * 
+	 * @param a Given a tree of integers.
+	 * @param x Given a integer.
+	 * @return delete the value x from a and return the resulting tree
+	 */
 	static Tree<Integer> delete(Tree<Integer> a, int x) {
 		if (a.isEmpty()) {
 			return null;
@@ -123,53 +185,146 @@ public class Worksheet2 implements Worksheet2Interface {
 				return new Tree<>(max(a.getLeft()), delete(a.getLeft(), max(a.getLeft())), a.getRight());
 			}
 		}
-
 	}
 
 	// Exercise 9
+	/**
+	 * method to see if it is height-balanced, returning a boolean value.
+	 * 
+	 * @param a Given a tree of integers.
+	 * @return a boolean value.
+	 */
 	static <E> boolean isHeightBalanced(Tree<E> a) {
-        if(a.isEmpty()) return true;
-        int left=getDepth(a.getLeft(),1);
-        int right=getDepth(a.getRight(),1);
-
-        return Math.abs(left-right)<=1&&isHeightBalanced(a.getLeft())&&isHeightBalanced(a.getRight());
+		if (a.isEmpty())
+			return true;
+		int left = getDepth(a.getLeft(), 1);
+		int right = getDepth(a.getRight(), 1);
+		return Math.abs(left - right) <= 1 && isHeightBalanced(a.getLeft()) && isHeightBalanced(a.getRight());
 
 	}
-	
-    static <E> int getDepth(Tree<E> a,int depth){
-        if(a.isEmpty()) return depth;
-        
-        int left=getDepth(a.getLeft(),depth+1);
-        int right=getDepth(a.getRight(),depth+1);
-        return Math.max(left,right);
-   
-    }
+	/**
+	 * method to get depth.
+	 * 
+	 * @param a Given a tree of integers.
+	 * @param root depth
+	 * @return tree depth.
+	 */
+	static <E> int getDepth(Tree<E> a, int depth) {
+		if (a.isEmpty())
+			return depth;
+		int left = getDepth(a.getLeft(), depth + 1);
+		int right = getDepth(a.getRight(), depth + 1);
+		return Math.max(left, right);
+
+	}
 
 	// Exercise 10
-
-	static Tree<Integer> insertHB(Tree<Integer> a, int x) {
-		return new Tree<Integer>();
-	}
-
-	static Tree<Integer> deleteHB(Tree<Integer> a, int x) {
-		return new Tree<Integer>();
-	}
-
-	static void printList(List<Integer> L) {
-		if (L.isEmpty())
-			return;
-		else {
-			System.out.println(L.getHead());
-			printList(L.getTail());
+	/**
+	 * method to LL rotation
+	 * 
+	 * @param a Given a tree of integers.
+	 * @return LL rotation tree
+	 */
+	private static Tree<Integer> leftLeft(Tree<Integer> a) {
+		if (a.isEmpty()) {
+			return new Tree<Integer>();
 		}
+		return new Tree<Integer>(a.getLeft().getValue(), a.getLeft().getLeft(),
+				new Tree<Integer>(a.getValue(), a.getLeft().getRight(), a.getRight()));
 	}
-
-	public static void main(String[] args) {
-		Tree a = new Tree<>(5, new Tree<>(3, new Tree<>(1), new Tree<>(4)), new Tree<>(8, new Tree<>(6), new Tree<>()));
-//		Tree.print(delete(a, 5));
-//		printList(preorder(a));
-		System.out.println(isHeightBalanced(a));
-//		printDescending(a);
+	/**
+	 * method to RR rotation
+	 * 
+	 * @param a Given a tree of integers.
+	 * @return RR rotation tree
+	 */
+	private static Tree<Integer> rightRight(Tree<Integer> a) {
+		if (a.isEmpty()) {
+			return new Tree<Integer>();
+		}
+		return new Tree<Integer>(a.getRight().getValue(),
+				new Tree<Integer>(a.getValue(), a.getLeft(), a.getRight().getLeft()), a.getRight().getRight());
 	}
+	/**
+	 * method to RL rotation
+	 * 
+	 * @param a Given a tree of integers.
+	 * @return RL rotation tree
+	 */
+	private static Tree<Integer> rightLeft(Tree<Integer> a) {
+		if (a.isEmpty()) {
+			return new Tree<Integer>();
+		}
+		return new Tree<Integer>(a.getRight().getLeft().getValue(),
+				new Tree<Integer>(a.getValue(), a.getLeft(), a.getRight().getLeft().getLeft()),
+				new Tree<Integer>(a.getRight().getValue(), a.getRight().getLeft().getRight(), a.getRight().getRight()));
+	}
+	/**
+	 * method to LR rotation
+	 * 
+	 * @param a Given a tree of integers.
+	 * @return LR rotation tree
+	 */
+	private static Tree<Integer> leftRight(Tree<Integer> a) {
+		if (a.isEmpty()) {
+			return new Tree<Integer>();
+		}
+		return new Tree<Integer>(a.getLeft().getRight().getValue(),
+				new Tree<Integer>(a.getLeft().getValue(), a.getLeft().getLeft(), a.getLeft().getRight().getLeft()),
+				new Tree<Integer>(a.getValue(), a.getLeft().getRight().getRight(), a.getRight()));
+	}
+	/**
+	 * method to balance tree
+	 * 
+	 * @param a Given a tree of integers.
+	 * @return balanced tree
+	 */
+	private static Tree<Integer> balance(Tree<Integer> a) {
+		if (a.isEmpty()) {
+			return new Tree<Integer>();
+		}
+		if (a.getLeft().getHeight() - a.getRight().getHeight() > 1) {
+			if (a.getLeft().getLeft().getHeight() > a.getLeft().getRight().getHeight()) {
+				return leftLeft(a);
+			} else {
+				return leftRight(a);
+			}
+		}
+		if (a.getRight().getHeight() - a.getLeft().getHeight() > 1) {
+			if (a.getRight().getLeft().getHeight() > a.getRight().getRight().getHeight()) {
+				return rightLeft(a);
+			} else {
+				return rightRight(a);
+			}
+		} else
+			return a;
+	}
+	/**
+	 * method to insert x to balanced tree
+	 * 
+	 * @param a Given a tree of integers.
+	 * @param x Given a integer.
+	 * @return inserted balanced tree
+	 */
+	static Tree<Integer> insertHB(Tree<Integer> a, int x) {
+		if (a.isEmpty()) {
+			return new Tree<Integer>(x);
+		}
+		if (x < a.getValue()) {
+			return balance(new Tree<>(a.getValue(), insertHB(a.getLeft(), x), a.getRight()));
+		} else {
+			return balance(new Tree<>(a.getValue(), a.getLeft(), insertHB(a.getRight(), x)));
+		}
 
+	}
+	/**
+	 * method to delete x to balanced tree
+	 * 
+	 * @param a Given a tree of integers.
+	 * @param x Given a integer.
+	 * @return deleted balanced tree
+	 */
+	static Tree<Integer> deleteHB(Tree<Integer> a, int x) {
+		return balance(delete(a, x));
+	}
 }
--- 2/Worksheet2Test.java
+++ 2/Worksheet2Test.java
@@ -1,14 +1,220 @@
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 /** @author 
- *  <PUT YOUR NAME HERE>
+ *  <Ruopeng Jiang>
  * This class contains the test cases for Worksheet2 solutions.
  *  <WRITE YOUR TEST CASES BELOW>
  */
 
 public class Worksheet2Test {
+	Tree<Integer> T1;
+	Tree<Integer> T2;
+	Tree<Integer> T3;
+	Tree<Integer> T4;
 
+	@BeforeEach
+	public void beforeEach() {
+
+
+	}
+	// test for exercise1 
+		@Test	
+		public void test1() {
+			Tree T1 = new Tree<>(5, new Tree<>(3, new Tree<>(1), new Tree<>(4)), new Tree<>(8, new Tree<>(6), new Tree<>()));
+			Tree<Integer> expected = new Tree<>(-5, new Tree<>(-3, new Tree<>(-1), new Tree<>(-4)), new Tree<>(-8, new Tree<>(-6), new Tree<>()));;
+			Tree<Integer> actual = Worksheet2.negateAll(T1);
+			assertEquals(expected, actual, "error in exercise1");
+		}
 		
+		@Test	
+		public void test2() {
+			Tree T2 = new Tree<>(10, new Tree<>(8), new Tree<>(12, new Tree<>(11), new Tree<>(15)));
+			Tree<Integer> expected = new Tree<>(-10, new Tree<>(-8), new Tree<>(-12, new Tree<>(-11), new Tree<>(-15)));
+			Tree<Integer> actual = Worksheet2.negateAll(T2);
+			assertEquals(expected, actual, "error in exercise1");
+		}
+		
+		@Test	
+		public void test3() {
+			Tree T3 = new Tree<>(-10, new Tree<>(-12, new Tree<>(-20), new Tree<>()), new Tree<>(0, new Tree<>(), new Tree<>(2)));
+			Tree<Integer> expected = new Tree<>(10, new Tree<>(12, new Tree<>(20), new Tree<>()), new Tree<>(0, new Tree<>(), new Tree<>(-2)));
+			Tree<Integer> actual = Worksheet2.negateAll(T3);
+			assertEquals(expected, actual, "error in exercise1");
+		}
+		
+		@Test	
+		public void test4() {
+			Tree T4 = new Tree<>(10, new Tree<>(80), new Tree<>(2));
+			Tree<Integer> expected = new Tree<>(-10, new Tree<>(-80), new Tree<>(-2));
+			Tree<Integer> actual = Worksheet2.negateAll(T4);
+			assertEquals(expected, actual, "error in exercise1");
+		}
+		
+		// test for exercise2
+		@Test
+		public void test5() {
+			Tree T4 = new Tree<>(10, new Tree<>(80), new Tree<>(2));
+			Boolean expected = true;
+			Boolean actual = Worksheet2.allEven(T4);
+			assertEquals(expected, actual, "error in exercise2");
+		}
+		
+		@Test
+		public void test6() {
+			Tree T1 = new Tree<>(5, new Tree<>(3, new Tree<>(1), new Tree<>(4)), new Tree<>(8, new Tree<>(6), new Tree<>()));
+			Boolean expected = false;
+			Boolean actual = Worksheet2.allEven(T1);
+			assertEquals(expected, actual, "error in exercise2");
+		}
+		
+		// test for exercise3
+		@Test
+		public void test7() {
+			Tree T4 = new Tree<>(10, new Tree<>(80), new Tree<>(2));
+			int expected = 1;
+			int actual = Worksheet2.depth(T4,80);
+			assertEquals(expected, actual, "error in exercise3");
+		}
+		@Test
+		public void test8() {
+			Tree T1 = new Tree<>(5, new Tree<>(3, new Tree<>(1), new Tree<>(4)), new Tree<>(8, new Tree<>(6), new Tree<>()));
+			int expected = -1;
+			int actual = Worksheet2.depth(T1,80);
+			assertEquals(expected, actual, "error in exercise3");
+		}
+		
+		// test for exercise4
+		@Test
+		public void test9() {
+			Tree T4 = new Tree<>(10, new Tree<>(80), new Tree<>(2));
+			List<Integer> expected = new List<Integer>(10,
+					new List<Integer>(80, new List<Integer>(2, new List<Integer>())));;
+			List<Integer> actual = Worksheet2.preorder(T4);
+			assertEquals(expected, actual, "error in exercise4");
+		}
+		@Test
+		public void test10() {
+			Tree T1 = new Tree<>(5, new Tree<>(3, new Tree<>(1), new Tree<>(4)), new Tree<>(8, new Tree<>(6), new Tree<>()));
+			List<Integer> expected = new List<Integer>(5,
+					new List<Integer>(3, new List<Integer>(1, new List<Integer>(4, new List<Integer>(8,new List<Integer>(6,new List<Integer>()))))));
+			List<Integer> actual = Worksheet2.preorder(T1);
+			assertEquals(expected, actual, "error in exercise4");
+		}
+		
+		// test for exercise5
+		
+		@Test
+		public void test11() {
+			Tree T4 = new Tree<>(10, new Tree<>(80), new Tree<>(2));
+			Boolean expected = false;
+			Boolean actual = Worksheet2.isSearchTree(T4);
+			assertEquals(expected, actual, "error in exercise5");
+		}
+		@Test
+		public void test12() {
+			Tree T1 = new Tree<>(5, new Tree<>(3, new Tree<>(1), new Tree<>(4)), new Tree<>(8, new Tree<>(6), new Tree<>()));
+			Boolean expected = true;
+			Boolean actual = Worksheet2.isSearchTree(T1);
+			assertEquals(expected, actual, "error in exercise5");
+		}
+		
+		// test for exercise7
+		@Test
+		public void test13() {
+			Tree T1 = new Tree<>(5, new Tree<>(3, new Tree<>(1), new Tree<>(4)), new Tree<>(8, new Tree<>(6), new Tree<>()));
+			int expected = 8;
+			int actual = Worksheet2.max(T1);
+			assertEquals(expected, actual, "error in exercise7");
+		}
+		@Test
+		public void test14() {
+			Tree T3 = new Tree<>(-10, new Tree<>(-12, new Tree<>(-20), new Tree<>()), new Tree<>(0, new Tree<>(), new Tree<>(2)));
+			int expected = 2;
+			int actual = Worksheet2.max(T3);
+			assertEquals(expected, actual, "error in exercise7");
+		}
+		
+		// test for exercise8
+		@Test	
+		public void test15() {
+			Tree T1 = new Tree<>(5, new Tree<>(3, new Tree<>(1), new Tree<>(4)), new Tree<>(8, new Tree<>(6), new Tree<>()));
+			Tree<Integer> expected = new Tree<>(5, new Tree<>(3, new Tree<>(1), new Tree<>(4)), new Tree<>(8, new Tree<>(), new Tree<>()));
+			Tree<Integer> actual = Worksheet2.delete(T1,6);
+			assertEquals(expected, actual, "error in exercise8");
+		}
+		@Test	
+		public void test16() {
+			Tree T1 = new Tree<>(5, new Tree<>(3, new Tree<>(1), new Tree<>(4)), new Tree<>(8, new Tree<>(6), new Tree<>()));
+			Tree<Integer> expected = new Tree<>(4, new Tree<>(3, new Tree<>(1), new Tree<>()), new Tree<>(8, new Tree<>(6), new Tree<>()));
+			Tree<Integer> actual = Worksheet2.delete(T1,5);
+			assertEquals(expected, actual, "error in exercise8");
+		}
+		@Test	
+		public void test17() {
+			Tree T1 = new Tree<>(5, new Tree<>(3, new Tree<>(1), new Tree<>(4)), new Tree<>(8, new Tree<>(6), new Tree<>()));
+			Tree<Integer> expected = new Tree<>(5, new Tree<>(3, new Tree<>(1), new Tree<>(4)), new Tree<>(6, new Tree<>(), new Tree<>()));
+			Tree<Integer> actual = Worksheet2.delete(T1,8);
+			assertEquals(expected, actual, "error in exercise8");
+		}
+		// test for exercise9
+		@Test
+		public void test18() {
+			Tree T4 = new Tree<>(10, new Tree<>(80), new Tree<>(2));
+			Boolean expected = true;
+			Boolean actual = Worksheet2.isHeightBalanced(T4);
+			assertEquals(expected, actual, "error in exercise9");
+		}
+		@Test
+		public void test19() {
+			Tree T5 = new Tree<>(10, new Tree<>(80,new Tree<>(5),new Tree<>()), new Tree<>());
+			Boolean expected = false;
+			Boolean actual = Worksheet2.isHeightBalanced(T5);
+			assertEquals(expected, actual, "error in exercise9");
+		}
+		// test for exercise10
+		@Test	
+		public void test20() {
+			Tree T1 = new Tree<>(5, new Tree<>(3, new Tree<>(1), new Tree<>(4)), new Tree<>(8, new Tree<>(6), new Tree<>()));
+			Tree<Integer> expected = new Tree<>(4, new Tree<>(3, new Tree<>(1), new Tree<>()), new Tree<>(8, new Tree<>(6), new Tree<>()));
+			Tree<Integer> actual = Worksheet2.deleteHB(T1,5);
+			assertEquals(expected, actual, "error in exercise10");
+		}
+		@Test	
+		public void test21() {
+			Tree T1 = new Tree<>(5, new Tree<>(3, new Tree<>(1), new Tree<>(4)), new Tree<>(8, new Tree<>(6), new Tree<>()));
+			Tree<Integer> expected = new Tree<>(5, new Tree<>(3, new Tree<>(1), new Tree<>(4)), new Tree<>(8, new Tree<>(), new Tree<>()));
+			Tree<Integer> actual = Worksheet2.deleteHB(T1,6);
+			assertEquals(expected, actual, "error in exercise10");
+		}
+		@Test	
+		public void test22() {
+			Tree T7 = new Tree<>(10, new Tree<>(9,new Tree(8),new Tree()), new Tree<>(11));
+			Tree<Integer> expected = new Tree<>(9, new Tree<>(8), new Tree<>(10));
+			Tree<Integer> actual = Worksheet2.deleteHB(T7,11);
+			assertEquals(expected, actual, "error in exercise10");
+		}
+		@Test	
+		public void test23() {
+			Tree T6 = new Tree<>(10, new Tree<>(9), new Tree<>());
+			Tree<Integer> expected = new Tree<>(9, new Tree<>(6), new Tree<>(10));
+			Tree<Integer> actual = Worksheet2.insertHB(T6,6);
+			assertEquals(expected, actual, "error in exercise10");
+		}
+		@Test	
+		public void test24() {
+			Tree T1 = new Tree<>(5, new Tree<>(3, new Tree<>(1), new Tree<>(4)), new Tree<>(8, new Tree<>(6), new Tree<>()));
+			Tree<Integer> expected = new Tree<>(5, new Tree<>(3, new Tree<>(1), new Tree<>(4)), new Tree<>(8, new Tree<>(6), new Tree<>(10)));
+			Tree<Integer> actual = Worksheet2.insertHB(T1,10);
+			assertEquals(expected, actual, "error in exercise10");
+		}
+		@Test	
+		public void test25() {
+			Tree T1 = new Tree<>(5, new Tree<>(3, new Tree<>(1), new Tree<>(4)), new Tree<>(8, new Tree<>(6), new Tree<>()));
+			Tree<Integer> expected = new Tree<>(5, new Tree<>(3, new Tree<>(1), new Tree<>(4)), new Tree<>(7, new Tree<>(6), new Tree<>(8)));
+			Tree<Integer> actual = Worksheet2.insertHB(T1,7);
+			assertEquals(expected, actual, "error in exercise10");
+		}
 }
